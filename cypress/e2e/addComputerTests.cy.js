685ab6e34f4d71804f1c019ed3bd218b40f6212b
Dimitar Pop-Trpev <dimitarpoptrpev@gmail.com>
2024-11-29 19:12:05 +0100

    convert the all tests to POM

--- cypress/e2e/addComputerTests.cy.js
+++ cypress/e2e/addComputerTests.cy.js
@@ -4,11 +4,34 @@ describe('Add Computer Tests', () => {
     require('../utilities/testSetup.cy')
     it('Verify that new computer is added', () => {
         computerDatabasePage.addComputer()
-        computerDatabasePage.sendComputerName()
-        computerDatabasePage.sendIntroducedDate()
-        computerDatabasePage.sendDiscontinuedDate()
-        computerDatabasePage.sendCompany()
+        computerDatabasePage.sendComputerName('Dimitar\'s PC') //Dimitar\'s PC
+        computerDatabasePage.sendIntroducedDate('2024-11-11') //2024-11-11
+        computerDatabasePage.sendDiscontinuedDate('2024-11-20') //2024-11-20
+        computerDatabasePage.sendCompany(3)  //3
         computerDatabasePage.createComputer()
         computerDatabasePage.elements.warningMsg().should('have.text','Done !  Computer Dimitar\'s PC has been created')
+      })
+      const computersList = require('../fixtures/computers.json')
+      computersList.forEach((computer) => {
+          it('Data Driven Testing - ' + computer.name, () => {
+              computerDatabasePage.addComputer()
+              computerDatabasePage.sendComputerName(computer.name)
+              computerDatabasePage.sendIntroducedDate(computer.introduced)
+              computerDatabasePage.sendDiscontinuedDate(computer.discontinued)
+              computerDatabasePage.sendCompany(computer.company)
+              computerDatabasePage.createComputer()
+              computerDatabasePage.elements.warningMsg().should('have.text','Done !  Computer ' + computer.name + ' has been created')
+          })
+      })
+      it('Data Driven Testing', function () {    
+        this.computersData.forEach((computer) => {
+          computerDatabasePage.addComputer()
+          computerDatabasePage.sendComputerName(computer.name)
+          computerDatabasePage.sendIntroducedDate(computer.introduced)
+          computerDatabasePage.sendDiscontinuedDate(computer.discontinued)
+          computerDatabasePage.sendCompany(computer.company)
+          computerDatabasePage.createComputer()
+          computerDatabasePage.elements.warningMsg().should('have.text','Done !  Computer ' + computer.name + ' has been created')
+        })
       })
 })
--- cypress/pages/computerDatabasePage.js
+++ cypress/pages/computerDatabasePage.js
@@ -6,25 +6,35 @@ class computerDatabasePage {
         discontinuedTxt : () => cy.get('#discontinued'),
         companyDropdown : () => cy.get('#company'),
         submitBtn : () => cy.get('[type=submit]'),
-        warningMsg : () => cy.get('.warning')
+        warningMsg : () => cy.get('.warning'),
+        searchBox : () => cy.get('#searchbox'),
+        searchSubmit : () => cy.get('#searchsubmit'),
+        rowCount : () => cy.get('table.computers tbody tr'),
+        getMessage : () => cy.get('#main')
     }
     addComputer() {
         this.elements.addBtn().click()
     }
-    sendComputerName() {
-        this.elements.nameTxt().type('Dimitar\'s PC')
+    sendComputerName(computerName) {
+        this.elements.nameTxt().type(computerName)
     }
-    sendIntroducedDate() {
-        this.elements.introducedTxt().type('2024-11-11')
+    sendIntroducedDate(introducedDate) {
+        this.elements.introducedTxt().type(introducedDate)
     }
-    sendDiscontinuedDate() {
-        this.elements.discontinuedTxt().type('2024-11-20')
+    sendDiscontinuedDate(discontinuedDate) {
+        this.elements.discontinuedTxt().type(discontinuedDate)
     }
-    sendCompany() {
-        this.elements.companyDropdown().select('3')
+    sendCompany(companyValue) {
+        this.elements.companyDropdown().select(companyValue)
     }
     createComputer() {
         this.elements.submitBtn().click()
+    }
+    sendSearchText() {
+        this.elements.searchBox().type('ACE')
+    }
+    searchComputers() {
+        this.elements.searchSubmit().click()
     }
 }
 
