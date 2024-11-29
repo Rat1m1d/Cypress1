8eccecd26c6636cab7ccd8711c9af59a85d5c5bf
Dimitar Pop-Trpev <dimitarpoptrpev@gmail.com>
2024-11-29 20:41:36 +0100

    add navigate method

--- cypress/e2e/addComputerTests.cy.js
+++ cypress/e2e/addComputerTests.cy.js
@@ -3,34 +3,34 @@ import computerDatabasePage from '../pages/computerDatabasePage'
 describe('Add Computer Tests', () => {
     require('../utilities/testSetup.cy')
     it('Verify that new computer is added', () => {
-        computerDatabasePage.addComputer()
-        computerDatabasePage.sendComputerName('Dimitar\'s PC') //Dimitar\'s PC
-        computerDatabasePage.sendIntroducedDate('2024-11-11') //2024-11-11
-        computerDatabasePage.sendDiscontinuedDate('2024-11-20') //2024-11-20
+        computerDatabasePage.clickButton(computerDatabasePage.strings.addButton)
+        computerDatabasePage.sendText(computerDatabasePage.strings.computerName, 'Dimitar\'s PC')   //sendComputerName('Dimitar\'s PC') //Dimitar\'s PC
+        computerDatabasePage.sendText(computerDatabasePage.strings.introduced, '2024-11-11')   //sendIntroducedDate('2024-11-11') //2024-11-11
+        computerDatabasePage.sendText(computerDatabasePage.strings.discontinued, '2024-11-20')   //sendDiscontinuedDate('2024-11-20') //2024-11-20
         computerDatabasePage.sendCompany(3)  //3
-        computerDatabasePage.createComputer()
+        computerDatabasePage.clickButton(computerDatabasePage.strings.submitButton)
         computerDatabasePage.elements.warningMsg().should('have.text','Done !  Computer Dimitar\'s PC has been created')
       })
       const computersList = require('../fixtures/computers.json')
       computersList.forEach((computer) => {
           it('Data Driven Testing - ' + computer.name, () => {
-              computerDatabasePage.addComputer()
-              computerDatabasePage.sendComputerName(computer.name)
-              computerDatabasePage.sendIntroducedDate(computer.introduced)
-              computerDatabasePage.sendDiscontinuedDate(computer.discontinued)
+              computerDatabasePage.clickButton(computerDatabasePage.strings.addButton)
+              computerDatabasePage.sendText(computerDatabasePage.strings.computerName, computer.name) //sendComputerName(computer.name)
+              computerDatabasePage.sendText(computerDatabasePage.strings.introduced, computer.introduced) //sendIntroducedDate(computer.introduced)
+              computerDatabasePage.sendText(computerDatabasePage.strings.discontinued, computer.discontinued) //sendDiscontinuedDate(computer.discontinued)
               computerDatabasePage.sendCompany(computer.company)
-              computerDatabasePage.createComputer()
+              computerDatabasePage.clickButton(computerDatabasePage.strings.submitButton)
               computerDatabasePage.elements.warningMsg().should('have.text','Done !  Computer ' + computer.name + ' has been created')
           })
       })
       it('Data Driven Testing', function () {    
         this.computersData.forEach((computer) => {
-          computerDatabasePage.addComputer()
-          computerDatabasePage.sendComputerName(computer.name)
-          computerDatabasePage.sendIntroducedDate(computer.introduced)
-          computerDatabasePage.sendDiscontinuedDate(computer.discontinued)
+          computerDatabasePage.clickButton(computerDatabasePage.strings.addButton)
+          computerDatabasePage.sendText(computerDatabasePage.strings.computerName, computer.name)
+          computerDatabasePage.sendText(computerDatabasePage.strings.introduced, computer.introduced)
+          computerDatabasePage.sendText(computerDatabasePage.strings.discontinued, computer.discontinued)
           computerDatabasePage.sendCompany(computer.company)
-          computerDatabasePage.createComputer()
+          computerDatabasePage.clickButton(computerDatabasePage.strings.submitButton)
           computerDatabasePage.elements.warningMsg().should('have.text','Done !  Computer ' + computer.name + ' has been created')
         })
       })
--- cypress/e2e/searchComputerTests.cy.js
+++ cypress/e2e/searchComputerTests.cy.js
@@ -4,12 +4,18 @@ describe('Computer Database Tests', () => {
     require('../utilities/testSetup.cy')
     it('Search for computers containing ACE in theit name', () => {
         let resultCount
-        computerDatabasePage.sendSearchText()
-        computerDatabasePage.searchComputers()
+        computerDatabasePage.sendText(computerDatabasePage.strings.searchBox, 'ACE')
+        // computerDatabasePage.searchComputers()
+        computerDatabasePage.clickButton(computerDatabasePage.strings.searchButton)
         computerDatabasePage.elements.rowCount().should('have.length', 6)
         computerDatabasePage.elements.getMessage().should('contain.text', 'computers found').then((elm) => {
           resultCount = parseInt(elm.text().split(' ')[0])
           computerDatabasePage.elements.rowCount().should('have.length', resultCount)
         })
+      })
+      it('Verify the third computer name in the list', () => {
+        computerDatabasePage.clickButton(computerDatabasePage.strings.nextButton, 5)
+        // computerDatabasePage.navigate(5)
+        computerDatabasePage.elements.thirdComputer().should('have.text', computerDatabasePage.strings.thirdComputerName)
       })
     })
